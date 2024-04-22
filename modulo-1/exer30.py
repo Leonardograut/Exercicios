754cf62ad70135790d261ba99f97992b9143b264
Leonardo Nascimento <98924684+Leonardograut@users.noreply.github.com>
2024-04-22 16:19:07 -0300

    exercicios cdd com python

---
+++ modulo-1/exer30.py
@@ -0,0 +1,10 @@
+
+result = 's'
+while result == 's' or result == 'S':
+ num = int(input("digite um numero"))
+ if num <1:
+    print(" numero negativo")
+ else:
+    print ("numero positivo")
+
+result = int(input("Dejesa realizar o novo calculo:"))
