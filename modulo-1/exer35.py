b346d5ee8bf067ae83bde6e9bede44e48a6d8871
Leonardo Nascimento <98924684+Leonardograut@users.noreply.github.com>
2024-04-22 16:45:55 -0300

    exercicio cdd

---
+++ modulo-1/exer35.py
@@ -0,0 +1,10 @@
+s = 0
+for x in range (4):
+    num = int(input('Digite o segundo numero'))
+    s = s + num
+
+media =num/4
+
+print('a  soma entre eles sao',s," e a media entre eles",media)
+
+""""""
