670bc4de51a938b1c97b0320df1f9cb1bdc2d0f5
Leonardo Nascimento <98924684+Leonardograut@users.noreply.github.com>
2024-04-22 16:30:00 -0300

    exercicio cdd python

---
+++ modulo-1/exer34.py
@@ -0,0 +1,11 @@
+base = int(input('Insira o valor da  base'))
+altura = int(input("Insira o valor da altura:"))
+area = (base*altura)/2
+print(area)
+
+
+"""faça um codigo que receba o valor da base e da aultra 
+de um triangulo e calcule a sua area usando a formula A =(basexAltura)/2
+
+
+"""
