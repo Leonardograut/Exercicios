13378ecddf5cf496ef67ee533eb2d8edb42d671e
Leonardo Nascimento <98924684+Leonardograut@users.noreply.github.com>
2024-04-22 16:17:34 -0300

    exercicio31

---
+++ modulo-1/exer31.py
@@ -0,0 +1,11 @@
+idade = int(input('Digite sua idade'))
+anoAtual = 2024
+mesAtual = 4
+
+
+if anoAtual >= mesAtual:
+    anoNascimento = (anoAtual - idade)-1
+else:
+    anoNascimento = anoAtual -idade
+
+print('o ano de nascimento e',anoNascimento)
