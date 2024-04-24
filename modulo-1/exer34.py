c2f63b843f3fb71707a0c6038d9822ae2661a5e2
Leonardo Nascimento <98924684+Leonardograut@users.noreply.github.com>
2024-04-24 16:21:07 -0300

    exercicio cdd

--- modulo-1/exer34.py
+++ modulo-1/exer34.py
@@ -1,7 +1,19 @@
-base = int(input('Insira o valor da  base'))
-altura = int(input("Insira o valor da altura:"))
-area = (base*altura)/2
-print(area)
+num =0
+while num !=3 :
+ num = int(input('Digite 1 para area do triangulo e  2 para Area do Quadrado e Digite 3 para  sair do sistema'))
+ if num == 1:
+  base = int(input('Insira o valor do triangulo'))
+  altura = int(input("Insira o valor da altura:"))
+  triangulo = (base*altura)/2
+  print("o valor  do triangulo ",triangulo)
+
+ elif num == 2:
+  base = int(input('Insira o valor do quadrado'))
+  quadrado = base **2
+  print("area do quadrado",quadrado)
+
+ elif num > 3:
+   print("Opcao invalida")
 
 
 """faça um codigo que receba o valor da base e da aultra 
