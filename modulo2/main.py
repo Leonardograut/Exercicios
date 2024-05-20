c49441b92c5b772a9ff10179dc4208481549d376
Leonardo Nascimento <98924684+Leonardograut@users.noreply.github.com>
2024-05-20 17:11:17 -0300

    Update main

--- modulo2/main.py
+++ modulo2/main.py
@@ -8,6 +8,14 @@ p1.falar()
 p1.dormir()
 
 
+p2 = Conta(2222,'Leonardo','Corrente')
+p2.ativarConta()
+p2.ativarConta()
+p2.depositar(222)
+p2.sacar(200)
 
+p3 = Retangulo()
+p3.calcularArea(10,10)
+p3.calcularPerimetro(10,5)
 
 
