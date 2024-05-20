29d623ff538fb9e6207a825c9a3f1fbd7f9fa98f
Leonardo Nascimento <98924684+Leonardograut@users.noreply.github.com>
2024-05-20 15:52:50 -0300

    Conta.py

--- modulo2/classes.py
+++ modulo2/classes.py
@@ -42,6 +42,46 @@ class Pessoa():
       print(f'{self.nome} parou de dormir e  acordou')
 
 
+class Conta():
+    def __init__(self, numero_conta, nome_cliente, tipo_conta):
+        self.numero_conta = numero_conta
+        self.nome_cliente = nome_cliente
+        self.tipo_conta = tipo_conta
+        self.saldo = 0
+        self.status = False
+
+    def depositar(self,valor):
+     self.saldo +=valor
+     print(f"Depósito de {valor:.2f} realizado com sucesso! Saldo atual:{self.saldo}")
+
+    def sacar(self,valor):
+      if self.saldo >= valor:
+         self.saldo -=valor
+         print(f"Saque realizado com sucesso ")
+      else:
+        print(f"Saldo insuficiente")
+
+
+    def verificarSaldo(self):
+        if self.status == True :
+            print("Conta ativada com sucesso")
+        else:
+            print("Saldo da  conta deve estar zerado para ativar a conta")
+
+
+    def ativarConta(self):
+       if self.saldo == 0  and not self.status:
+          self.status = True
+          print('Conta ativada com sucesso')
+       elif self.saldo !=0:
+            print('Saldo da conta  deve ta zerado para ativar a conta')
+       else:
+            print('Conta ja esta ativada')
+
+    def desativarConta(self):
+       if self.saldo == 0:
+          self.status = False
+          print("Conta desativada com sucesso")
 
 
 
